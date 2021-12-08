5df04abb7980afab5c48a75d6775f922e0e23c60
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2021-12-07 20:01:34 -0800

    2020 day 15 solutions

---
+++ 2020/day15a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from collections import Counter
+import sys
+
+for r in map(str.strip, sys.stdin):
+    nums = list(map(int, r.split(",")))
+
+    endturn = 2020
+
+    lastsaid = Counter()
+    for i, n in enumerate(nums):
+        lastsaid[n] = i + 1
+
+    turn = len(nums) + 1
+    lastnum = nums[-1]
+    lastsaid[lastnum] = 0
+
+    # print(lastsaid)
+    while turn <= endturn:
+        say = 0
+        # print(turn, lastnum, lastsaid[lastnum])
+        if lastsaid[lastnum]:
+            say = (turn - 1) - lastsaid[lastnum]
+        lastsaid[lastnum] = turn - 1
+        # print(f"X{say}X")
+        lastnum = say
+        turn = turn + 1
+
+    print(lastnum)
