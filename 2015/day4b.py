db5ff04aa0b406d673627286c3f114cee772cd7d
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2022-02-15 18:34:01 -0800

    2015 day 4 solutions

---
+++ 2015/day4b.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from hashlib import md5
+import sys
+
+key = sys.stdin.readline().strip()
+
+seeking = "000000"
+i = 1
+while True:
+    if md5((key + str(i)).encode()).hexdigest().startswith(seeking):
+        break
+    i = i + 1
+
+print(i)
