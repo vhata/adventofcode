d29bb4d732d0c191e2981227e23988bcfc010aee
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2022-02-18 14:24:45 -0800

    2015 day 12 solutions

---
+++ 2015/day12b.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+
+def tally(th):
+    if isinstance(th, int):
+        return th
+    if isinstance(th, str):
+        return 0
+    if isinstance(th, list):
+        return sum(tally(t) for t in th)
+    if isinstance(th, dict):
+        if "red" in th.values():
+            return 0
+        return sum(tally(th[k]) for k in th)
+
+
+total = 0
+for r in map(str.strip, sys.stdin):
+    thing = json.loads(r)
+    total = total + tally(thing)
+
+print(total)
