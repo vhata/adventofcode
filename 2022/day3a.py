8dc131cc7a1ed460661446eed48e4a515b0e2170
Jonathan Hitchcock <jonathan.hitchcock@discordapp.com>
2022-12-05 13:14:37 -0800

    Python 3 doh

--- 2022/day3a.py
+++ 2022/day3a.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
 extras = []
 for r in map(str.strip, sys.stdin):
-    extra = list(set(r[:len(r)/2]).intersection(set(r[len(r)/2:])))[0]
+    extra = list(set(r[:len(r)//2]).intersection(set(r[len(r)//2:])))[0]
     if extra < 'a':
         extra = chr(ord('z') + ord(extra) - ord('A') + 1)
     extras.append(extra)
