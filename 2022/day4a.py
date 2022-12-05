b8ad4d12fcef9d36687944fecf80dc70bbcbb424
Jonathan Hitchcock <jonathan.hitchcock@discordapp.com>
2022-12-05 13:24:55 -0800

    Use maps!

--- 2022/day4a.py
+++ 2022/day4a.py
@@ -7,7 +7,7 @@ overlaps = 0
 for r in map(str.strip, sys.stdin):
     m = re.match(r"^(\d+)-(\d+),(\d+)-(\d+)", r)
     assert m
-    p1a, p1b, p2a, p2b = [int(p) for p in m.groups()]
+    p1a, p1b, p2a, p2b = map(int, m.groups())
     if (p1a >= p2a and p1b <= p2b) or (p2a >= p1a and p2b <= p1b):
         overlaps = overlaps + 1
 
--- 2022/day4b.py
+++ 2022/day4b.py
@@ -7,7 +7,7 @@ overlaps = 0
 for r in map(str.strip, sys.stdin):
     m = re.match(r"^(\d+)-(\d+),(\d+)-(\d+)", r)
     assert m
-    p1a, p1b, p2a, p2b = [int(p) for p in m.groups()]
+    p1a, p1b, p2a, p2b = map(int, m.groups())
     if (
         (p1a <= p2a <= p1b)
         or (p1a <= p2b <= p1b)
